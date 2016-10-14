6af8e69fc50151439fb16e4e1a0bb14fc03c4a92
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-10-14 00:07:15 -0400

    ADD a class attempt one

--- main.py
+++ main.py
@@ -4,6 +4,64 @@
 
 import nflgame
 
+class AttemptOne:
+    playerDict = {}
+    playerCount = 0
+
+
+    def setPlayerDict(self):
+        total = []
+        for year in range(2009, 2016):
+            season = nflgame.games(year)
+            total = total + season
+
+        players = nflgame.combine_game_stats(total)
+        curId = 0
+        for p in players:
+            if (not self.playerDict.get(p.playerid)):
+                self.playerDict[p.playerid] = curId
+                curId += 1
+
+        self.playerCount = curId
+        # Outputs 3694
+
+
+    def runDataAcq():
+        # TEMPORARILY REDUCE THE NUMBERS SO IT RUNS FASTER
+        # for year in range(2009, 2016):
+        #    season = nflgame.games(year)
+        for week in range(1,15):
+            season = nflgame.games(2013, week=week);
+            for game in season:
+                game = [game]
+                players = nflgame.combine_game_stats(game)
+                
+                # Would be best to calculate the number of players dynamically
+                playerArray = [0] * 3694;
+                for p in players.filter(home=True):
+                    if (not playerDict.get(p.playerid)):
+                        playerDict[p.playerid] = curId
+                        curId += 1
+                    playerArray[curId] = 1
+
+                print playerArray
+
+
+
+                print 
+                print
+                #for p in players.rushing().sort("rushing_yds").limit(10):
+                #    print p
+                #for p in players.sort('id'):
+                    #print p, p.playerid
+        
+    
+    def __init__(self):
+        # Ensures that this dict is only set up once
+        if (not self.playerDict):   
+            print "setting"
+            self.setPlayerDict()
+
 
 
 # class DataPoint:
@@ -20,39 +78,9 @@ import nflgame
     # I will attempt at a smarter implementation when I try this out
 
 def main():
-    attemptOne()
-
-def attemptOne():
-    playerDict = {}
-    curId = 0
-
-    # TEMPORARILY REDUCE THE NUMBERS SO IT RUNS FASTER
-    # for year in range(2009, 2016):
-    #    season = nflgame.games(year)
-    for week in range(1,15):
-        season = nflgame.games(2013, week=week);
-        for game in season:
-            game = [game]
-            players = nflgame.combine_game_stats(game)
-            
-            # Would be best to calculate the number of players dynamically
-            playerArray = [0] * 3694;
-            for p in players.filter(home=True):
-                if (not playerDict.get(p.playerid)):
-                    playerDict[p.playerid] = curId
-                    curId += 1
-                playerArray[curId] = 1
-
-            print playerArray
+    att = AttemptOne()
 
 
-
-            print 
-            print
-            #for p in players.rushing().sort("rushing_yds").limit(10):
-            #    print p
-            #for p in players.sort('id'):
-                #print p, p.playerid
 
 
 def aggregateData():
